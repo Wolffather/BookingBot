d34aee9021d50fa96c8c842f27d5118f13904731
Wolffather <maxcross93@gmail.com>
2026-03-23 00:03:19 +0300

    Code cleanup

--- src/main/kotlin/ru/hey_savvy/bookingBot/bot/router/UpdateRouter.kt
+++ src/main/kotlin/ru/hey_savvy/bookingBot/bot/router/UpdateRouter.kt
@@ -3,21 +3,15 @@ package ru.hey_savvy.bookingBot.bot.router
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.meta.api.objects.Update
 import ru.hey_savvy.bookingBot.bot.BotCommand
-import ru.hey_savvy.bookingBot.bot.MessageSender
-import ru.hey_savvy.bookingBot.bot.handler.ServiceHandler
 import ru.hey_savvy.bookingBot.bot.handler.StartHandler
 import ru.hey_savvy.bookingBot.bot.state.ConversationStateManager
 import ru.hey_savvy.bookingBot.bot.state.Step
-import kotlin.ranges.step
 
 @Component
 class UpdateRouter(
     private val stateManager: ConversationStateManager,
-    //private val serviceHandler: ServiceHandler,
     private val startHandler: StartHandler,
-//    private val bookingHandler: BookingHandler,
-//    private val myBookingsHandler: MyBookingsHandler,
-//    private val cancelHandler: CancelHandler,
+
 ) {
     fun route(update: Update) {
 
